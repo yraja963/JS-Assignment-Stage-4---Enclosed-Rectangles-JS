f7bd268feccd2876bfa0d51f95a8d3eb34dd684f
yraja963 <61427586+yraja963@users.noreply.github.com>
2020-11-03 16:14:24 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+//rec = {
+//	top: '25px',
+//	left: '96px',
+//	width: '64px',
+//	height: '96px'
+//}
+
+function updateStructure(rec1,rec2){
+	//write your code
+}
+
+module.exports = updateStructure;
