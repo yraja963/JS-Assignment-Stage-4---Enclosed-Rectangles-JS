b5956db46c3384a1f9c6c2fc10829d53a98f62d1
yraja963 <yraja@gmail.com>
2020-11-22 11:56:29 +0530

    answer

--- src/components/index.js
+++ src/components/index.js
@@ -7,6 +7,59 @@
 
 function updateStructure(rec1,rec2){
 	//write your code
+	if(contains(recA,recB)){
+		const relativeDim=relative(recA,recB);
+		return {...recA,children: [relativeDim] };
+	}elseif (contains(recB,recA)) {
+		const relativeDim=relative(recB,recA);
+	    return {...recB, children: [relativeDim]};
+	}else {
+		return {...recA};
+	}
+}
+function relative(recA,recB){
+	const recAn=normalize(recA);
+	const recBn=normalize(recB);
+	const res = {
+		children: recB.children
+	}
+	if(recB.top) {
+		res.top = `${recBn.x1-recAn.x1}px`;
+	}
+	if(recB.left){
+		res.left = `${recBn.y1-recAn.y1}px`;
+	}
+	if(recB.height) {
+		res.height = recB.height;
+	}
+	if(recB.bottom) {
+		res.bottom = `${recAn.x2-recBn.x2}px`;
+	}
+	if(recB.right) {
+		res.right = `${recAn.y2-recBn.y2}px`;
+	}
+	return res;
+}
+
+function contains(recA,recB) {
+	const recAn=normalize(recA);
+	const recBn = normalize(recB);
+	if(
+		recAn.x1 <= recBn.x1 && recAn.y1 <= recBn.y1 && recAn.x2 >= recBn.x2 && recAn.y2 >= recBn.y2
+	){
+		return true;
+	}
+	return false;
+}
+const T=0;
+const W=0;
+function normalize(rec) {
+	return{
+		x1:rec.top ? parseInt(rec.top) : (T-(parseInt(rec.bottom) + parseInt(rec.height))),
+		y1:rec.left ? parseInt(rec.left) : (W-(parseInt(rec.right) + parseInt(rec.width))),
+		x2:rec.bottom ? (T-parseInt(rec.bottom)) : (parseInt(rec.top) + parseInt(rec.height)),
+		y2:rec.right ? (W-parseInt(rec.right)) : (parseInt(rec.left) + parseInt(rec.width))
+	}
 }
 
 module.exports = updateStructure;
