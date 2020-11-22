c1239a696d5109472a9eecde5d5c4ba43e2d0f99
yraja963 <yraja@gmail.com>
2020-11-22 12:29:08 +0530

    answer

--- src/components/index.js
+++ src/components/index.js
@@ -27,10 +27,10 @@ function relative(recA,recB) {
 	}
 
 	if(recB.top) {
-		res.top = `${recBn.x1-recAn.x1}px`;
+		res.top = `${recBn.x1 - recAn.x1}px`;
 	}
 	if(recB.left){
-		res.left = `${recBn.y1-recAn.y1}px`;
+		res.left = `${recBn.y1 - recAn.y1}px`;
 	}
 	if(recB.height) {
 		res.height = recB.height;
@@ -39,18 +39,22 @@ function relative(recA,recB) {
 		res.width = recB.width;
 	}
 	if(recB.bottom) {
-		res.bottom = `${recAn.x2-recBn.x2}px`;
+		res.bottom = `${recAn.x2 - recBn.x2}px`;
 	}
 	if(recB.right) {
-		res.right = `${recAn.y2-recBn.y2}px`;
+		res.right = `${recAn.y2 - recBn.y2}px`;
 	}
 	return res;
 }
 
 function contains(recA,recB) {
-	const recAn=normalize(recA);
+	const recAn = normalize(recA);
 	const recBn = normalize(recB);
-	if(recAn.x1 <= recBn.x1 && recAn.y1 <= recBn.y1 && recAn.x2 >= recBn.x2 && recAn.y2 >= recBn.y2) {
+
+	if(  recAn.x1 <= recBn.x1 
+		&& recAn.y1 <= recBn.y1 
+		&& recAn.x2 >= recBn.x2 
+		&& recAn.y2 >= recBn.y2) {
 		return true;
 	}
 	return false;
@@ -59,10 +63,10 @@ const T=0;
 const W=0;
 function normalize(rec) {
 	return {
-		x1:rec.top ? parseInt(rec.top) : (T-(parseInt(rec.bottom) + parseInt(rec.height))),
-		y1:rec.left ? parseInt(rec.left) : (W-(parseInt(rec.right) + parseInt(rec.width))),
-		x2:rec.bottom ? (T-parseInt(rec.bottom)) : (parseInt(rec.top) + parseInt(rec.height)),
-		y2:rec.right ? (W-parseInt(rec.right)) : (parseInt(rec.left) + parseInt(rec.width))
+		x1:rec.top ? parseInt(rec.top) : (T - (parseInt(rec.bottom) + parseInt(rec.height))),
+		y1:rec.left ? parseInt(rec.left) : (W - (parseInt(rec.right) + parseInt(rec.width))),
+		x2:rec.bottom ? (T - parseInt(rec.bottom)) : (parseInt(rec.top) + parseInt(rec.height)),
+		y2:rec.right ? (W - parseInt(rec.right)) : (parseInt(rec.left) + parseInt(rec.width))
 	}
 }
 
